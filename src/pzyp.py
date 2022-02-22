4eb98afac34f05546e6f0bd06a0cc4eb877760a3
Filipe Cavaco <fec.bertrand@gmail.com>
2022-02-22 17:18:06 +0000

    Merge branch 'main' of https://github.com/mjoaoclaro/pzyp

--- src/pzyp.py
+++ src/pzyp.py
@@ -1,25 +1,24 @@
 
-from curses import window
-import sys
 from collections import deque
 import os
+from docopt import docopt
+import time
+import struct
 
-
-FILE_EXTENTION = 'lzs' 
 
 """
 This is a work of our python class, we're implementing a compressor/decompressor using the LZSS method
 
     Usage:
-        pzyp.py [-c | -d |(<FILE>)
+        pzyp.py (-c [-l LEVEL] | -d) [-sh] [-p PASSWORD] FILE
 
     Options: (some of the options will be implemented after)
-        -h, --help         show this text
-        -c, --compress     compress FILE
-        -d, --decompress   decompress FILE
-        -l                 compressing LEVEL [default: 2]
-        -s, --sumary       meta-info of compressed FILE
-        -p, --password     establishes a PASSWORD for FILE encription
+        -h, --help                  show this text
+        -c, --compress              compress FILE
+        -d, --decompress            decompress FILE
+        -l, --comprlevel=LEVEL      compressing LEVEL [default: 2]
+        -s, --sumary                meta-info of compressed FILE
+        -p, --password=PASSWORD     establishes a PASSWORD for FILE encription
 
     LEVEL: is an int that ranges between 1 and 4; the
     compression LEVEL affects the window dimension(size of buffer)
@@ -44,21 +43,31 @@ This is a work of our python class, we're implementing a compressor/decompressor
     
     
 """
+LEVEL = {1: (10, 4), 2: (12, 4), 3: (14, 5), 4: (15, 5)}
+
+ARGS = docopt(__doc__)
+
+FILE_EXTENTION = 'lzs'
+
+ENCODED_OFFSET_SIZE=LEVEL[int(ARGS['--comprlevel'])][0]
+
+
+class PzypError(ValueError):
+    pass
 
 
 def importFile(location):
-    file = open(location, 'r+')
-    content = file.read()
-    file.close()
+    with open(location, 'rb+') as file:
+        content = file.read()
     return content
 
 def exportFile(compressedText, extension):
-    head, tail = os.path.split(sys.argv[2])
+    head, tail = os.path.split(ARGS['FILE'])
     file=str(tail)
     fileName=file.split('.')
-    newFile = open(f'{fileName[0]}.{extension}', 'w')
-    newFile.write(compressedText)
-    newFile.close()
+    with open(f'{fileName[0]}.{extension}', 'ab') as newFile:
+        newFile.write(compressedText)
+
 
 class Window:
     def __init__(self):
@@ -151,32 +160,53 @@ class Window:
             position += 1
         return result
 
-        
+def head_writer(level_nb, file_name):
+    win_dimention = level_nb[0]
+    max_seq = level_nb[1]
+    dt = time.time()
+    with open(f'{file_name}.copia', 'wb+') as f:
+        f.write(struct.pack('>iid', win_dimention, max_seq, dt))
+        f.write(struct.pack('{}s'.format(len(file_name)), bytes(file_name, 'utf-8')))
+        print(struct.calcsize('> i i d'))
+        print(struct.calcsize('{}s'.format(len(file_name))))
 
+def head_reader(file_name):
+    with open(file_name, 'rb') as f:
+        s_list=list(struct.unpack('> i i d 9s', f.read()))
+    dt_sec = s_list[2]
+    dt = time.ctime(dt_sec)
+    print(f'File name: {str(s_list[3])}')
+    print(f'Compression date/time:  {dt}')
+    print(f'Compression parameters : Buffer -> {2**s_list[0]} ({s_list[0]} bits),')
+    if ARGS['--comprlevel'] == 1:
+        print('Max Seq. Len. -> 17 (4 bits)')
+    elif ARGS['--comprlevel'] == 2:
+        print('Max Seq. Len. -> 18 (4 bits)')
+    else:
+        print('Max Seq. Len. -> 35 (5 bits)')
 
-
-
-
-
-if __name__ == '__main__':
-    
-#   args = docopt('__doc__') / para usar nas fases a seguir
-
-    if len(sys.argv) < 3:
-        print(f"Usage: python3 {sys.argv[0]} -c (for compress) file.txt or - d (for decompress) file.lsz]")
-        sys.exit(2)
-
-    if sys.argv[1] == '-c':
-            textContent = importFile(sys.argv[2])
+def main():
+    if ARGS['--compress']:
+            head_writer(LEVEL[int(ARGS['--comprlevel'])], ARGS['FILE'])
+            textContent = importFile(ARGS['FILE'])
             encodeWindow = Window()
             result = encodeWindow.encode(textContent)
-            exportFile(result, FILE_EXTENTION)
-    
-    if sys.argv[1] == '-d':
-            textToDecompress = importFile(sys.argv[2])
+            if ARGS['--comprlevel']:
+                print("teste")
+            exportFile(result, FILE_EXTENTION)         
+    elif ARGS['--decompress']:
+            textToDecompress = importFile(ARGS['FILE'])
             decodeWindow = Window()
             decompressedResult = decodeWindow.decode(textToDecompress)
             exportFile(decompressedResult, 'txt')
+    if ARGS['--password']:
+        print("colocar aqui o codigo em vez deste print")
+    if ARGS['--sumary']:
+        head_reader(ARGS['FILE'])
+
+if __name__ == '__main__':
+    main()
+    
 
  
 
